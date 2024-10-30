e73a50bf99ccf3ebc1d899f02ea0046f60e91799
xkirsa00 <xkirsa00@vutbr.cz>
2024-10-30 15:57:59 +0100

    cp7

---
+++ cp7/Core/Inc/sct.h
@@ -0,0 +1,21 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 9, 2024
+ *      Author: xkirsa00
+ *
+ *      Header of my sct lib.
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#include "main.h"
+
+
+void sct_init();
+void sct_led(uint32_t value);
+void sct_value(uint16_t value);
+
+
+#endif /* INC_SCT_H_ */
